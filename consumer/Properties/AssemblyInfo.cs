b2b72cc92cf18fd8d56f0f1028ba298a2b9d0772
dbobrovreal <dbobrovreal@mail.ru>
2020-12-21 22:50:09 +0700

    init commit

---
+++ consumer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("consumer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("consumer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("d45e866e-687b-4c9c-aa60-d5a48f6b5489")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using AForge.Video.DirectShow;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Net.Sockets;
+
+namespace producer
+{
+    class Program
+    {
+        private static IPEndPoint consumerIPEngPoint;
+        private static UdpClient udpclient = new UdpClient();
+        static void Main(string[] args)
+        { 
+            var consumerIp = ConfigurationManager.AppSettings.Get("consumerIP");
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerIPEngPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+            Console.WriteLine($"consumer: { consumerIPEngPoint}");
+            FilterInfoCollection videoDivece = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDivece[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+
+            Console.ReadLine();
+
+        }
+
+        private static void VideoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            var bmp = new Bitmap(eventArgs.Frame, 1025, 667);
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bmp.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpclient.Send(bytes, bytes.Length, consumerIPEngPoint);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
